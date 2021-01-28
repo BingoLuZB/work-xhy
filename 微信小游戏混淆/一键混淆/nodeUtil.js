6ca0a2141a261d6e9da14c6feae57b31275f7134
BingoLuZB <38916948+BingoLuZB@users.noreply.github.com>
2021-01-29 02:09:26 +0800

    一键混淆
    整理了代码，新增了start之后打开html，以及在html发送数据给node的功能

---
+++ 微信小游戏混淆/一键混淆/nodeUtil.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const cp = require('child_process');
+const http = require("http");
+module.exports = {
+    // 展示打包完成
+    showAlert() {
+        cp.exec('msg %username% "打包完成"')
+    },
+
+    // 读取文件
+    rf(path) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, 'utf-8', (err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    },
+
+    // 用浏览器打开index.html
+    openHtml() {
+        // 打开html
+        return new Promise((resolve, reject) => {
+            http.createServer((req, res) => {
+                fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
+                    if (err) {
+                        res.statusCode = 404;
+                        res.setHeader("Content-Type", "text/plain");
+                        res.end("Not Found!");
+                        reject()
+                    }
+                    else {
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", "text/html");
+                        res.end(data);
+                    }
+                });
+            }).listen(3000, "localhost", () => {
+                console.log("Successfully");
+                cp.exec('start http://localhost:3000');
+            });
+            // 接收请求
+            http.createServer(function (req, res) {
+                if (req.url !== "/favicon.ico") {
+                    req.on('data', function (data) {
+                        const resData = decodeURIComponent(data)
+                        console.log("服务器接收到的数据：　" + resData);
+                        resolve(resData)
+                    });
+                    req.on("end", function () {
+                        console.log('客户端请求数据全部接收完毕');
+                    });
+                }
+                res.end();
+            }).listen(8080, "localhost", function () {
+                console.log("listened");
+            });
+        })
+
+    }
+
+}
--- 微信小游戏混淆/一键混淆/start.js
+++ 微信小游戏混淆/一键混淆/start.js
@@ -1,52 +1,49 @@
 const fs = require('fs');
-const path = require('path');
+const readline = require('readline');
+const http = require("http");
 const cp = require('child_process');
-const packageIdArr = [666, 55 ,11]
+const { openHtml } = require('./nodeUtil')
+// const packageIdArr = [666, 55 ,11]
+
 const jsonCopyStr = {
     key: 'newBuild-key',
     value: 'newBuild-value'
 }
-function showAlert() {
-    cp.exec('msg %username% "打包完成"')
+let inputConfig = {}
+
+async function init() {
+    // 打开网页，并拿到用户输入的数据
+    let res = await openHtml()
+    console.log(res)
 }
 
-function rf(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
 
-// 新增webpackConfig
+
+// 新增webpackConfig package.json
 function addWebpackConfig() {
     // 读取config里面的配置文件
     let allStr = "concurrently "
-    packageIdArr.forEach((item, index)=> {
-        const name =  `mj${item}`
+    packageIdArr.forEach((item, index) => {
+        const name = `mj${item}`
+        // 新建webpack.config.js
         // const configData = fs.readFileSync(`./config/config_${name}.js`, 'utf-8')
         // const webpackData = fs.readFileSync('./moduleFile/webpack.config-module.js', 'utf-8')
         // const data = configData + webpackData;
         // fs.writeFileSync(`webpack.config-${name}.js`, data, {flag: 'w+'})
 
         // 配置package.json
-        let webpackStr = `\\"set NODE_ENV=build&webpack --config webpack.config-${name}.js\\" `
-        allStr+=webpackStr
-        if (index == packageIdArr.length-1) {
+        let webpackStr = `\\"npm run set NODE_ENV=build&webpack --config webpack.config-${name}.js\\" `
+        allStr += webpackStr
+        if (index == packageIdArr.length - 1) {
             let packageJsonData = fs.readFileSync('./moduleFile/packageModule.json', 'utf-8')
             packageJsonData = packageJsonData.replace(jsonCopyStr.key, 'all').replace(jsonCopyStr.value, allStr)
             fs.writeFileSync('package.json', packageJsonData, 'utf8')
-            console.log(packageJsonData);
         }
     });
-    
+
 }
 
-addWebpackConfig()
+// addWebpackConfig()
 
 // async function test() {
 //     try {
