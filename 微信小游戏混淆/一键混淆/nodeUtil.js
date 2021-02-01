f7a6f22f677d46197b30542b79081f2d0c15f564
BingoLuZB <38916948+BingoLuZB@users.noreply.github.com>
2021-02-02 01:13:26 +0800

    一键混淆工具

--- 微信小游戏混淆/一键混淆/nodeUtil.js
+++ 微信小游戏混淆/一键混淆/nodeUtil.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const cp = require('child_process');
 const http = require("http");
 const httpUrl = 'localhost'
+const querystring = require('querystring')
 module.exports = {
     // windows 警告框
     showAlert(str) {
@@ -23,6 +24,7 @@ module.exports = {
 
     // 用浏览器打开index.html
     openHtml() {
+        let fileNames = fs.readdirSync('inputGame')
         // 打开html
         return new Promise((resolve, reject) => {
             http.createServer((req, res) => {
@@ -47,9 +49,44 @@ module.exports = {
             http.createServer(function (req, res) {
                 if (req.url !== "/favicon.ico") {
                     req.on('data', function (data) {
-                        const resData = decodeURIComponent(data)
+                        let inputConfig = {}
+                        let resData = decodeURIComponent(data)
                         console.log("服务器接收到的数据：　" + resData);
-                        resolve(resData)
+                        resData = querystring.parse(resData);
+                        if (typeof resData.game !== 'string' && resData.game.length > 1) {
+                            // 如果有多个打包配置
+                            for (let i = 0; i < resData.game.length; i++) {
+                                let obj = {}
+                                for(let j in resData) {
+                                    obj[j] = resData[j][i]
+                                }
+                                inputConfig[obj.game] = obj
+                            }
+                        } else {
+                            // 如果只有1个打包配置
+                            inputConfig[resData.game] = resData
+                        }
+                        for (let i in inputConfig) {
+                            if (fileNames.includes(i)) {
+                                let item = inputConfig[i]
+                                for (let j in item) {
+                                    switch (j) {
+                                        // 处理要混淆文件的格式
+                                        case 'file':    
+                                            console.log(item.j);
+                                            break;
+                                        // 把用户输入的游戏id变成数组
+                                        case 'idArr':
+                                            console.log(item.j);
+                                            break;
+                                    }
+                                }
+                            } else {
+                                cp.exec(`msg %username% inputGame目录下没有游戏：${i}`)
+                                return false
+                            }
+                        }
+                        resolve(inputConfig)
                     });
                     req.on("end", function () {
                         console.log('客户端请求数据全部接收完毕');
--- 微信小游戏混淆/一键混淆/start.js
+++ 微信小游戏混淆/一键混淆/start.js
@@ -14,52 +14,22 @@ const jsonCopyStr = {
 // 用户选择的配置
 let inputConfig = {}
 
+
+
+init()
+
+
 // 初始化
 async function init() {
     // 打开网页，并拿到用户输入的数据
     try {
-        let res = await openHtml()
-        // 把字符串切割成多个数组
-        let resArr = res.split('&')
-        let obj = {}
-        let gameName = ''
-        resArr.forEach((item, index) => {
-            let arr = item.split('=')
-            switch (arr[0]) {
-                case 'game':
-                    gameName = arr[1]
-                    break;
-                case 'file':
+        inputConfig = await openHtml()
 
-                    break;
-                case 'idArr':
-
-                    break;
-            }
-            obj[arr[0]] = arr[1]
-            // 一个配置里面有8个项目
-            if (gameName && (index + 1) % 8 === 0) {
-                fs.readdir('inputGame', function (err, files) {
-                    if (err) {
-                        return console.log('目录不存在')
-                    } else {
-                        // 检查inputGame目录下有没有游戏
-                        if (files.includes(gameName)) {
-                            inputConfig[gameName] = obj
-                            obj = {}
-                        } else {
-                            showAlert(`inputGame目录下不存在游戏：${gameName}`)
-                        }
-                    }
-                })
-            }
-        })
     } catch (error) {
-        console.error(error)
+        console.error(error, '===error`')
     }
 }
 
-init()
 
 
 // 新增webpackConfig package.json
